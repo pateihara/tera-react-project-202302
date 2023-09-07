c9b513795b06c9268bc1bf4874bf8ffe5d94a08a
Patricia Eihara <pateihara@gmail.com>
2023-09-07 18:49:37 -0300

    adiciona tela inicial parte 1

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../../images/logo.svg";
 
 export default function Home() {
-  return (
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://63cf09718a780ae6e6710dbe.mockapi.io/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setIsloading(false);
+      });
+  }, []);
+
+  return isLoading ? (
+    <h1>loading...</h1>
+  ) : (
     <div className="home center">
       <div className="home__logo">
         <img src={logo} className="responsive" alt="" />
       </div>
       <select className="home__select-users">
-        <option>User 1</option>
-        <option>User 2</option>
-        <option>User 3</option>
+        {users
+          .sort((a, b) => a.fn.localeCompare(b.fn))
+          .map((user) => (
+            <option value={user.id} key={user.id}>
+              {user.fn} {user.ln}
+            </option>
+          ))}
       </select>
       <button className="button-primary">Entrar</button>
     </div>
