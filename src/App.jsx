c9a3b619074ea5f4f33fa0be78624f2383e57020
Patricia Eihara <pateihara@gmail.com>
2023-09-07 16:10:57 -0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./components/pages/Home";
+import Users from "./components/pages/Users";
+import UserBlog from "./components/pages/UserBlog";
+import PostForm from "./components/pages/PostForm";
+
 import "./styles/normalize.css";
 import "./styles/fontawesome.min.css";
 import "./styles/main.css";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:userId" element={<UserBlog />} />
+        <Route path="/users/:userId/post" element={<PostForm />} />
+        <Route path="*" element={<h1>NotFound</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
